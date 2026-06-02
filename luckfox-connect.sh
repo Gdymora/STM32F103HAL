b22c30fd3d228db59d631c0a7f3fb8356e45455e
Alexandr <alekspetrofint@gmail.com>
2026-06-02 13:01:24 +0300

    luckfox

--- luckfox-connect.sh
+++
@@ -1,5 +0,0 @@
-# luckfox-connect.sh
-IFACE=$(ip link | grep "enx" | awk '{print $2}' | tr -d ':')
-sudo ip addr add 172.32.0.100/24 dev $IFACE
-sudo ip link set $IFACE up
-sudo nmap -sn 172.32.0.0/24 -e $IFACE | grep "report for"
